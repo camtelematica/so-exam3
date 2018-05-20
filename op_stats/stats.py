dd37fca93d8be847dfc4f62fa5fa0a38b3b19537
DonMiguelin <miguel111andres@hotmail.com>
2018-05-20 18:00:20 -0500

    Change some files

--- op_stats/stats.py
+++ op_stats/stats.py
@@ -3,17 +3,17 @@ import psutil
 class Stats():
 
   @classmethod
-  def porcentaje_cpu(cls):
+  def dar_porcentaje_cpu(cls):
     porcentaje = psutil.cpu_percent()
     return porcentaje
 
   @classmethod
-  def memoria_disponible(cls):
+  def dar_memoria_disponible(cls):
     ram_disponible = psutil.virtual_memory().available
     return ram_disponible
 
   @classmethod
-  def espacio_disco(cls):
+  def dar_espacio_disco(cls):
     espacio_disponible = psutil.disk_usage('/').free
     return espacio_disponible
 
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,4 +1,7 @@
 import pytest
+import sys
+sys.path.append('/home/flaskdev/so-exam3')
+
 from op_stats.app import app
 from op_stats.stats import Stats
 
@@ -8,20 +11,21 @@ def client():
   return client
 
 def test_get_cpu_percent(mocker, client):
-  mocker.patch.object(Stats, 'porcentaje_cpu', return_value=100)
+  mocker.patch.object(Stats, 'dar_porcentaje_cpu', return_value=100)
   response = client.get('/v1/stats/cpu')
   assert response.data.decode('utf-8') == '{"Porcentaje CPU": 100}'
   assert response.status_code == 200
 
 def test_get_available_memory(mocker, client):
-  mocker.patch.object(Stats, 'memoria_disponible', return_value=1000)
+  mocker.patch.object(Stats, 'dar_memoria_disponible', return_value=2000)
   response = client.get('/v1/stats/memory')
-  assert response.data.decode('utf-8') == '{"Memoria disponible": 1000}'
+  assert response.data.decode('utf-8') == '{"Memoria Disponible": 2000}'
   assert response.status_code == 200
 
 
 def test_get_disk_space(mocker, client):
-  mocker.patch.object(Stats, 'espacio_disco', return_value=2000)
+  mocker.patch.object(Stats, 'dar_espacio_disco', return_value=1000)
   response = client.get('/v1/stats/disk')
-  assert response.data.decode('utf-8') == '{"Espacio Libre Disco": 2000}'
+  assert response.data.decode('utf-8') == '{"Espacio Libre Disco": 1000}'
   assert response.status_code == 200
+
